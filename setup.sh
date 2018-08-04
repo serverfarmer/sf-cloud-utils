250e997aaf767131b9e24e52156e3ef11301e74e
Tomasz Klim <github@tomaszklim.pl>
2018-08-04 13:01:16 +0200

    require sf-cloud-client-ec2 extension

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+/opt/farm/scripts/setup/extension.sh sf-cloud-client-ec2
+
 if ! grep -q /opt/farm/ext/cloud-utils/cron /etc/crontab; then
 	echo "setting up crontab entry"
 	echo "35 * * * * root /opt/farm/ext/cloud-utils/cron/test-accounts.sh" >>/etc/crontab
