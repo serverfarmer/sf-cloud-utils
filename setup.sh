4b69fd2a6e62093e9e2f2113000894d30cedde27
Tomasz Klim <github@tomaszklim.pl>
2018-10-11 22:15:20 +0200

    removed all code from deprecated extension

--- setup.sh
+++ setup.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
-echo "skipping setup (deprecated extension)"
-exit 0
-
-/opt/farm/scripts/setup/extension.sh sf-cloud-client-ec2
-/opt/farm/scripts/setup/extension.sh sf-cloud-client-azure
-
-if ! grep -q /opt/farm/ext/cloud-utils/cron /etc/crontab; then
-	echo "setting up crontab entry"
-	echo "35 * * * * root /opt/farm/ext/cloud-utils/cron/test-accounts.sh" >>/etc/crontab
+if grep -q /opt/farm/ext/cloud-utils/cron /etc/crontab; then
+	sed -i -e "/\/opt\/farm\/ext\/cloud-utils\/cron/d" /etc/crontab
 fi
+
+echo "skipping setup (deprecated extension)"
--- uninstall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-if grep -q /opt/farm/ext/cloud-utils/cron /etc/crontab; then
-	sed -i -e "/\/opt\/farm\/ext\/cloud-utils\/cron/d" /etc/crontab
-fi
